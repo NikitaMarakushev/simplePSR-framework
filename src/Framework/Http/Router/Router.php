fe37839fc8c15546a5ff104c6cb2ae7c6dd4c770
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-02-28 14:00:47 +0300

    Result class based

---
+++ src/Framework/Http/Router/Router.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Framework\Http\Router;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+class Router
+{
+    private $routes;
+
+    public function __construct(RouteCollection $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function match(ServerRequestInterface $request): Result
+    {
+
+    }
+
+}
